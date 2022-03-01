bdfd30e929ce9a877321f70d125deb702a31afc8
UnityMaster0 <adam.munch@gmail.com>
2022-03-01 17:39:49 -0500

    init

---
+++ functions.py
@@ -0,0 +1,8 @@
+from sklearn import impute
+
+
+def my_function():
+  print("Hello from a function")
+  print("This is coming from a function")
+
+my_function()
---
+++ lambda.py
@@ -0,0 +1,3 @@
+x = lambda a, b, c : a*b*c
+
+print(x(3, 4, 2))
