8ccfd9554dbde2087c007e8f166f3a70f56f1280
UnityMaster <adam.munch@gmail.com>
2023-02-28 22:05:06 -0500

    Update Main.java

--- Java/Main.java
+++ Java/Main.java
@@ -2,12 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static double func(double value1, double value2) {
-        var sum = value1 + value2;
-        return sum;
-    }
-
-    public static void main(String[] arg) {
+    public static double add() {
         System.out.println("Hello, World");
         
         Scanner listener1 = new Scanner(System.in);
@@ -15,11 +10,16 @@ public class Main {
         String s1 = listener1.nextLine();
         System.out.println("What is your second value?: ");
         String s2 = listener1.nextLine();
-
-        float v1 = Float.parseFloat(s1);
-        float v2 = Float.parseFloat(s2);
-
-        System.out.println(func(v1, v2));
         listener1.close();
+
+        double v1 = Double.parseDouble(s1);
+        double v2 = Double.parseDouble(s2);
+
+        var sum = v1 + v2;
+        return sum;
+    }
+    
+    public static void main(String[] arg) {
+        System.out.println(add());
     }
 }
